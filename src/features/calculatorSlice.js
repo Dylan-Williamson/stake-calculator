f48bd4670c331dd3f7c9c76eaf3f86ce0edf650f
github username <dirtyassweed@icloud.com>
2021-04-15 14:01:53 -0400

    initial commit

---
+++ src/features/calculatorSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+export const calculatorSlice = createSlice({
+  name: 'calculator',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+  },
+});
+
+export const { increment } = calculatorSlice.actions;
+
+
+export default calculatorSlice.reducer;
